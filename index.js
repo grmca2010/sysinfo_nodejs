b43d558879e73ddf130908ee0d23146af7eda578
sgundepa <sgundepa@cisco.com>
2015-04-10 15:24:26 +0530

    modified service

--- index.js
+++ index.js
@@ -4,45 +4,21 @@ var app  = express();
 var path = require("path");
 var port = process.env.PORT || 80;
 var exec = require('child_process').exec,child,responseResult={};
+var routes=require("./routes/api");
 
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(path.normalize(__dirname + '../../../../../../'))); // grab root dev-ops
+// CORS
 
-function generateResponse(response){
-	response.writeHead(200, {"Content-Type": "application/json"});
-	 var json = JSON.stringify(responseResult);
-	 response.end(json);
-	
-}
-
-app.get("/api/status",function(request,response){
-    //finding total space-total space and available space
-	child = exec(' df -h  / | awk \'NR>1 {print $2 "-" $5}\'',  function (error, stdout, stderr) {	
-		if (error !== null) {
-			console.log('exec error: ' + error);
-		}
-		responseResult.diskspace=stdout;
-		
-		//find status of local instance of pupet process id and start time
-		var puppetcmd=exec('ps -ef | grep "puppet" | awk \'{print $2"-"$5}\'',  function (error, stdout, stderr) {	
-			if (error !== null) {
-				console.log('exec error: ' + error);
-			}
-			responseResult.puppet=stdout;	
-			//find status of local instance of jenkins process id and start time
-			var jenkinscmd=exec('ps -ef | grep "jenkin" | grep 8080| awk \'{print $2"-"$5}\'',  function (error, stdout, stderr) {	
-				if (error !== null) {
-					console.log('exec error: ' + error);
-				}
-				responseResult.jenkins=stdout;	
-				//call this function to generate response
-				generateResponse(response);
-			});
-		});
-		
-	});
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
 });
+
+app.get('/api/status',routes.status);
+
 app.listen(port);
 
 console.log('Server running on port ' + port);
